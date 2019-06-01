31efdb3e69edbc08754ebd332245316ab8f6c191
Ruchitesh Malukani <ruchiteshmalukani@gmail.com>
2019-06-01 17:56:02 +0530

    Setup for authentication

--- opinion_mining.py
+++ opinion_mining.py
@@ -1,3 +1,15 @@
+import  get_secrets # User defined class for getting keys and tokens
 import re
-name = "RucHiTesh"
-print(re.sub(r"[A-Z]","*",name))
+import tweepy
+from tweepy import OAuthHandler
+consumer_key=get_secrets.get_secrets.get_consumer_key()
+consumer_secret = get_secrets.get_secrets.get_consumer_secret()
+access_token = get_secrets.get_secrets.get_access_token()
+access_token_secret =get_secrets.get_secrets.get_access_token_secret()
+try:
+    auth = OAuthHandler(consumer_key,consumer_secret)
+    auth.set_access_token(access_token,access_token_secret)
+    api=tweepy.API(auth)
+except Exception as e:
+    print(ex)
+
