7535b413301651a3a8e93800fc19713d93ceceb6
Ruchitesh Malukani <41920841+Rpmalukani22@users.noreply.github.com>
2019-06-01 23:35:55 +0530

    Keys and Access Tokens

---
+++ get_secrets.py
@@ -0,0 +1,19 @@
+class get_secrets:
+    def __init__(self):
+        pass
+    @staticmethod
+    def get_consumer_key():
+        return "Consumer key here"
+
+    @staticmethod
+    def get_consumer_secret():
+        return "Consumer secret here"
+
+    @staticmethod
+    def get_access_token():
+        return "Access token here"
+
+    @staticmethod
+    def get_access_token_secret():
+        return "Token secret here"
+
