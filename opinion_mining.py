3c81f59a0dba4dce08206a4e8a0dc4fd7b097531
Ruchitesh Malukani <ruchiteshmalukani@gmail.com>
2019-06-01 23:30:59 +0530

    Sentiment Analysis completed

--- opinion_mining.py
+++ opinion_mining.py
@@ -4,54 +4,73 @@ Objective of application : Determine whether a piece of writing is positive, neg
 """
 
 # imports
-import  get_secrets # Contains user defined class retrieving getting keys and tokens
+import get_secrets  # Contains user defined class retrieving getting keys and tokens
 import re
 import tweepy
 from tweepy import OAuthHandler
 import pprint
-from textblob import TextBlob #NLP
+from textblob import TextBlob  # NLP
 
 
-def Cleaner(tweetbody):
-    print("__"*50+"Original"+"__"*50)
+# Clean the tweet text
+def cleaner(tweetbody):
+    print("__" * 50 + "Original" + "__" * 50)
     print(tweetbody)
     print("__" * 50 + "Cleaned" + "__" * 50)
-    regex = r"(@\w+)" # Find user tags(for example @user)
-    tweetbody=''.join(re.sub(regex,"",tweetbody)) # Remove matched pattern
-    regex = r"(\w+:\/\/\S+)" #find links
-    tweetbody=''.join(re.sub(regex,"",tweetbody))#remove links
+    regex = r"(@\w+)"  # Find user tags(for example @user)
+    tweetbody = ''.join(re.sub(regex, "", tweetbody))  # Remove matched pattern
+    regex = r"(\w+:\/\/\S+)"  # find links
+    tweetbody = ''.join(re.sub(regex, "", tweetbody))  # remove links
     regex = r"(#\w+)"  # Find hash tags(for example #NZvSL)
     tweetbody = re.sub(regex, "", tweetbody)  # Replace matched pattern with space
-    regex = r"[^0-9A-z \t]" # Find special characters
-    tweetbody=''.join(re.sub(regex,"",tweetbody)) # remove special characters
+    regex = r"[^0-9A-z \t]"  # Find special characters
+    tweetbody = ''.join(re.sub(regex, " ", tweetbody))  # remove special characters
     regex = r"(\s)"
-    tweetbody = ''.join(re.sub(regex," ", tweetbody))
-    tweetbody=" ".join(tweetbody.split()) # Multiple space characters with single space character
+    tweetbody = ''.join(re.sub(regex, " ", tweetbody))
+    tweetbody = " ".join(tweetbody.split())  # Multiple space characters with single space character
     print(tweetbody)
+    print("__" * 100)
+    return tweetbody
+
+
+def get_tweet_sentiment(tweet):
+    analysis = TextBlob(cleaner(tweet))
+    print("__"*50+"Results"+"__"*50)
+    if analysis.sentiment.polarity > 0:
+        print("Positive Tweet")
+    elif analysis.sentiment.polarity == 0:
+        print("Neutral Tweet")
+    else:
+        print("Negative Tweet")
     print("__"*100)
+
 
 # query : search query
 # count : number of tweets to be fetched
-def get_tweets(query,count):
-    tweets=[]
-    fetched_data = api.search(q=query,count=count)
+def get_tweets(query, count):
+    tweets = []
+    fetched_data = api.search(q=query, count=count)
     for tweet in fetched_data:
         # print(tweet.text) #fetched tweet
         tweets.append(tweet.text)
     return tweets
 
-consumer_key=get_secrets.get_secrets.get_consumer_key()
+
+# set credentials
+
+consumer_key = get_secrets.get_secrets.get_consumer_key()
 consumer_secret = get_secrets.get_secrets.get_consumer_secret()
 access_token = get_secrets.get_secrets.get_access_token()
-access_token_secret =get_secrets.get_secrets.get_access_token_secret()
+access_token_secret = get_secrets.get_secrets.get_access_token_secret()
+
+# main task
 try:
-    auth = OAuthHandler(consumer_key,consumer_secret)
-    auth.set_access_token(access_token,access_token_secret)
-    api=tweepy.API(auth)
-    query=input("Search Query : ")
-    tweets = get_tweets(query,10)
-    for tweetbody in tweets:
-        Cleaner(tweetbody)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    query = input("Search Query : ")
+    tweets = get_tweets(query, 15)
+    for tweet in tweets:
+        get_tweet_sentiment(tweet)
 except Exception as e:
     print(e)
-
